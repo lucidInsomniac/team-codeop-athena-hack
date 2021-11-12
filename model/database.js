0a07ce064941ef3677f70fdee499594a51de40c8
Donna Chin <donnchin@gmail.com>
2021-11-12 11:29:36 -0500

    corrected database migration msg

--- model/database.js
+++ model/database.js
@@ -22,7 +22,7 @@ con.connect(function(err) {
   let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `puzzles` was successful!");
+    console.log("Your table was created successfully! Way to Go!");
 
     console.log("Closing...");
   });
